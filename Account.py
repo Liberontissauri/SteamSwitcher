70487ce1705cfdf7f5430f6d9383bdc87a3ca4af
Liberontissauri <vaz.liberato@gmail.com>
2020-02-16 16:13:26 +0000

    Password Saving
    register - Saves an username and a password into a file;
    
    getpass - Returns the password for a user registered.

--- Account.py
+++ Account.py
@@ -4,3 +4,22 @@ def login(user, password):
     os.system("taskkill.exe /F /IM steam.exe")
     os.chdir("C:\Program Files (x86)\Steam")
     os.system("start steam.exe -login {} {}".format(user, password))
+
+def register(user,password):
+
+    path = "config/users/{}.ini".format(user)
+    f = open( path , "w")
+    f.write(user)
+    f.write("\n")
+    f.write(password)
+    f.close
+
+def getpass(user):
+
+    path = "config/users/{}.ini".format(user)
+    f = open( path , "r")
+    f.readline()
+    password = f.readline()
+    f.close
+    return str(password)
+    
